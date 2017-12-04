e106863d780b0e81d1b75fb30e2fcff278e5e3ee
rexdf <rexdf@vip.qq.com>
2017-12-04 21:50:30 +0800

    fix for win7 64 start 32bit application

--- CommandTrayHost/configure.cpp
+++ CommandTrayHost/configure.cpp
@@ -1094,7 +1094,7 @@ void create_process(
 
 	// https://stackoverflow.com/questions/53208/how-do-i-automatically-destroy-child-processes-in-windows
 	// Launch child process - example is notepad.exe
-	if (false == require_admin && CreateProcess(NULL, commandLine, NULL, NULL, FALSE, CREATE_NEW_CONSOLE, NULL, working_directory, &si, &pi))
+	if (false == require_admin && CreateProcess(NULL, commandLine, NULL, NULL, FALSE, CREATE_NEW_CONSOLE | CREATE_BREAKAWAY_FROM_JOB, NULL, working_directory, &si, &pi))
 	{
 		jsp["handle"] = reinterpret_cast<int64_t>(pi.hProcess);
 		jsp["pid"] = static_cast<int64_t>(pi.dwProcessId);
